9d141c1baa086ac51d0404a03bcd3e1d2d21f574
Bowen She <deltaever9@gmail.com>
2015-11-14 01:43:05 -0600

    add comments and summerization to enumType

--- src/playWithJava/enumType.java
+++ src/playWithJava/enumType.java
@@ -9,9 +9,13 @@ public class enumType {
 		d =Day.WED;
 		System.out.println(d);
 		Day t = Day.valueOf("WED");
+		// Day t = Day.valueOf("NODAY"); This line of code will be thought as error during running time
 		System.out.println(t);
 		System.out.println((int)c);
-		
+		/* primitive cast from char to int will change the char value into its ASCII value if it's a non-digit
+		 * A safer cast method named Character.getNumericValue(int) is helpful if you only want to cast a digit char into a int number
+		 * If you do not assign anything to the char variable, its int value is 0;
+		 */
 
 	}
 
